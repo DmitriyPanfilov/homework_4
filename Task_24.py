a3b9533dcbbff8b4d92f0bcbf8abc5a357d16ebe
Dmitriy <Dipan187@live.ru>
2023-07-06 16:36:17 +0300

    Task 24

--- Task_24.py
+++ Task_24.py
@@ -16,14 +16,9 @@ else:
     arr = []
     for i in range(n):
         arr.append(int(input('Введите количество ягод: ')))
-
-    first_num = arr[0] + arr[1] + arr[n-1]
-    last_num = arr[0] + arr[n-2] + arr[n-1]
     max_num = 0
-    if first_num > last_num:
-        max_num = first_num
-    else: max_num = last_num    
-    for i in range(1, n-1):
-        if max_num < arr[i] + arr[i-1] + arr[i+1]:
-            max_num = arr[i] + arr[i-1] + arr[i+1]
+   
+    for i in range(len(arr)):
+        if arr[i] + arr[i-1] + arr[(i+1) % len(arr)] > max_num:
+            max_num = arr[i] + arr[i-1] + arr[(i+1) % len(arr)]
     print(max_num)
